71e516bff2565cc3939eefa145e93faffdebcaff
Kobatai <taiga1983346@gmail.com>
2019-06-03 22:28:21 +0900

    create_controller

---
+++ app/controllers/image_recog_controller.rb
@@ -0,0 +1,24 @@
+class ImageRecogController < ApplicationController
+  def index
+    @image = AiAnalysisLog.new
+
+  end
+
+  def create
+    @image  = AiAnalysisLog.new(image_params)
+    if @image.save
+      respond_to do |format|
+        format.html { redirect_to :root }
+        format.json { render json: @image}
+      end
+    else
+      render :index
+  end
+
+
+  private
+
+  def image_params
+    params.require(:ai_analysis_log).permit(:image)
+  end
+end
--- app/models/ai_analysis_log.rb
+++ app/models/ai_analysis_log.rb
@@ -1,2 +1,3 @@
 class AiAnalysisLog < ApplicationRecord
+
 end
