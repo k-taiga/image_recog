a74110d04d898a615d85fc6551940b28ee500a09
Kobatai <taiga1983346@gmail.com>
2019-06-08 21:18:32 +0900

    add apipost

--- app/controllers/image_recog_controller.rb
+++ app/controllers/image_recog_controller.rb
@@ -1,6 +1,5 @@
 class ImageRecogController < ApplicationController
   def index
-    # @image = AiAnalysisLog.new
 
   end
 
--- app/models/ai_analysis_log.rb
+++ app/models/ai_analysis_log.rb
@@ -1,3 +1,18 @@
 class AiAnalysisLog < ApplicationRecord
 
+  def apipost
+
+    uri = URI.parse("https://：http://example.com/image_path/")
+    http = Net::HTTP.new(uri.host, uri.port)
+
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+    req = Net::HTTP::Post.new(uri.path)
+    req.set_form_data({'name' => 'hoge', 'content' => 'hogehoge'})
+
+    res = http.request(req)
+
+  end
+
 end
